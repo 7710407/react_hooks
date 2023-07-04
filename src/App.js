61a97f09296b4cb82362cdb4c9237100771f7973
Arsen Davtyan <Arsen.Davtyan@microchip.com>
2023-07-04 14:00:31 +0400

    Added useEffect hook

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import StateTutorial1 from "./UseState/StateTutorial1"
 import StateTutorial2 from "./UseState/StateTutorial2"
 import StateTutorial3 from "./UseState/StateTutorial3"
 import ReducerTutorial from "./UseReducer/ReducerTutorial"
+import EffectTutorial from './UseEffect/EffectTutorial';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <StateTutorial2 />
       <StateTutorial3 />
       <ReducerTutorial />
+      <EffectTutorial />
     </div>
   );
 }
